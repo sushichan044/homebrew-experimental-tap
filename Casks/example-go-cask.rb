4f60e86b45c65bd7afe02f0bb0f7202b33dfca0f
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-06-05 00:10:22 +0900

    owa

--- Casks/example-go-cask.rb
+++ Casks/example-go-cask.rb
@@ -72,7 +72,7 @@ cask "example-go-cask" do
 
     pp stage_path
 
-    system_command "/usr/bin/xattr", args: ["-d", "com.apple.quarantine", "#{Homebrew.bin}/example-go-cask"]
+    system_command "/usr/bin/xattr", args: ["-d", "com.apple.quarantine", "#{Homebrew.prefix.join("bin")}/example-go-cask"]
   end
 
   # No zap stanza required
