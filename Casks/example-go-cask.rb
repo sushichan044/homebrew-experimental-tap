d04c91443d04ee161a1191077aa78145c4cdbc15
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-06-05 00:37:25 +0900

    drs

--- Casks/example-go-cask.rb
+++ Casks/example-go-cask.rb
@@ -66,7 +66,7 @@ cask "example-go-cask" do
 
   postflight do
     if system_command("/usr/bin/xattr", args: ["-h"]).exit_status == 0
-      system_command "/usr/bin/xattr", args: ["-d", "-r", "-s", "com.apple.quarantine", "#{caskroom_path}/#{version}/example-go-cask"]
+      system_command "/usr/bin/xattr", args: ["-drs", "com.apple.quarantine", "#{caskroom_path}/#{version}/example-go-cask"]
     end
   end
 
